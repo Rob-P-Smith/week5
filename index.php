c807e45f2ef1ffa3550424fc2c4bc02578dd1a1f
Rob Smith <smith.rob@student.greenriver.edu>
2024-02-06 12:18:45 -0500

    Day 2 Lessons Added

--- index.php
+++ index.php
@@ -11,9 +11,14 @@ $f3 = Base::instance();
 
 $f3->route('GET /', function ($f3) {
 
+    $f3->set('veggies',array('tomato','corn','pea'));
+    $f3->set('fruits',array('apple','orange','banana'));
+    $f3->set('radius','10');
+    $f3->set ('color','purple');
     $f3->set ('username','jshmo');
     $f3->set ('password',sha1('Password01'));
     $f3->set ('title','Working with templates');
+    $f3->set('food', 'Sushi');
     $view = new Template();
     echo $view->render('views/home.html');
 });
